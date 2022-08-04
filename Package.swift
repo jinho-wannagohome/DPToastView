0f525754be3078aad27e76395b3b5b145cab44e0
jinho-wannagohome <jinho.jang@wesang.com>
2022-08-04 12:24:26 +0900

    support spm

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DPToastView",
+    products: [
+        .library(name: "DPToastView", targets: ["DPToastView"]),
+    ],
+    targets: [
+        .target(name: "DPToastView", path: ".")
+    ]
+)

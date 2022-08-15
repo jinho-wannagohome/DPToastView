b2e86ff5b250b0c51815c77a2100961a4bd08bc4
jinho-wannagohome <jinho.jang@wesang.com>
2022-08-15 10:09:41 +0900

    remove Package.swift from target

--- Package.swift
+++ Package.swift
@@ -9,6 +9,6 @@ let package = Package(
         .library(name: "DPToastView", targets: ["DPToastView"]),
     ],
     targets: [
-        .target(name: "DPToastView", path: ".")
+        .target(name: "DPToastView", path: ".", exclude: ["Package.swift"])
     ]
 )
